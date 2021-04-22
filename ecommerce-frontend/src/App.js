cf6ea5f67ecca609baf469d59b04909650003526
Pankaj <pvatwani@enphaseenergy.com>
2021-04-23 00:10:53 +0530

    Setting Material UI and Header and Footer Components

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,7 +1,14 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+
 function App() {
 	return (
 		<div className="App">
-			<h1>hello</h1>
+			<Header />
+			<main>
+				<h1>hello</h1>
+			</main>
+			<Footer />
 		</div>
 	);
 }
